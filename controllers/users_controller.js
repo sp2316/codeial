d8e8b168b50a991c420db5a3d1875cb0bb108c51
Sai prasad <sp@MacBook-Pro.local>
2021-01-11 16:00:41 +0530

    Add Sign out and make website little generic

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -55,6 +55,12 @@ if(req.body.password != req.body.confirm_password){
 // sign in and create a session for the user
 module.exports.createSession=function(req,res){
     
-    return res.redirect('/users/profile');
+    return res.redirect('/');
     
+}
+
+
+module.exports.destroySession= function(req,res){
+    req.logout(); //passport method
+    return res.redirect('/');
 }
