997d981e6528fcb579f561ace7af7fd6c65bd809
Sai prasad <sp@MacBook-Pro.local>
2021-01-22 20:13:46 +0530

    Add Posted by user name by using .populate method

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,16 +4,20 @@ const Post = require("../models/posts");
 module.exports.home=function(req,res){
    
     if(req.isAuthenticated()){ //if logged in sign up page shouldnt be shown
-         Post.find({},function(err,posts){
-            return res.render('home', {
-                title:'Home',
-                posts: posts
-            });
-         
-         });
-        }     
-        else
-     return res.redirect('/users/sign-up');
+         //populate the user of each post
+     Post.find({}).populate('user').exec(function(err,posts){
+        //  'user' is the name of the attribute in the post database
+        return res.render('home', {
+            title:'Home',
+            posts: posts
+        });
 
+     })
+
+}
+else{ 
+    return res.redirect('/users/sign-up');
+
+}
 }
 // module.exports.actionName = function(req,res){}
