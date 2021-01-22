3a9a513f96b8199ef8a397666b5b81c445ce31e6
Sai prasad <sp@MacBook-Pro.local>
2021-01-22 19:38:35 +0530

    Add code to view posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,19 @@
+const Post = require("../models/posts");
+
+
 module.exports.home=function(req,res){
+   
     if(req.isAuthenticated()){ //if logged in sign up page shouldnt be shown
-        return res.render('home', {title:'Home'});
-     }
+         Post.find({},function(err,posts){
+            return res.render('home', {
+                title:'Home',
+                posts: posts
+            });
+         
+         });
+        }     
+        else
      return res.redirect('/users/sign-up');
+
 }
 // module.exports.actionName = function(req,res){}
