e5c5033e8b0f8b821b8ed0a923a41322e9f8c419
Sai prasad <sp@MacBook-Pro.local>
2021-01-27 14:45:16 +0530

    delete a post (authorized)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require("../models/posts")
+const Comment = require("../models/comment")
+
 
 module.exports.create=function(req, res){
     Post.create({
@@ -11,4 +13,27 @@ module.exports.create=function(req, res){
         }
         return res.redirect('back');
     });
+}
+
+module.exports.destroy = function(req,res){
+// Before deleting find if post exists in db
+// format of url  is /posts/destroy/id ---> String param
+
+Post.findById(req.params.id,function(err,post){
+// check if user who created the post is the one deleting it
+// post.user requires a string id
+    if(post.user == req.user.id) {//convert ._id(object id) to string by using .id directly
+     post.remove(); 
+//  Delete comments associated with that post
+     Comment.deleteMany({post:req.params.id},function(err){
+         return res.redirect('back');
+     });
+    }else{
+        return res.redirect('back');
+    }
+});
+
+
+
+
 }
