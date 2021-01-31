ed203fd9eab9443b1f3f9a0c6c7758082fa8b613
Sai prasad <sp@MacBook-Pro.local>
2021-01-31 16:04:13 +0530

    Receive data from server using AJAX and JQuery

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,7 +10,8 @@
                 url:'/posts/create',
                 data:newPostForm.serialize(), //this converts post form data into json (content:value)
                 success:function(data){
-                    console.log(data);
+                    let newPost= newPostDom(data.data.post);
+                    $('#posts-list-container > ul').prepend(newPost);
                 },
                 error:function(error){
                     console.log(err.responseText);
@@ -22,7 +23,41 @@
 
         });
 
-
+    }
+    // method to create a post in DOM
+    let newPostDom=function(post){
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        <!-- if user is signed in and the user who signed in is same as user who created the post -->
+                        <small>
+                                <!-- post.id or post._id  both works the same -->
+                                <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a> 
+                        </small>
+                        
+                        ${post.content}
+                    <br>
+                    <small>
+                    Posted by ${post.user.name}
+                    </small>
+                    </p>
+                    <div class="post-comments">
+    
+                        <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type here to add Comment..." required>   
+                                <input type="hidden" name="post" value="${post._id}">
+                                <input type="submit" value="Add Comment"> 
+                        
+                        </form>
+                        
+                        <div class="post-comments-list">
+                                <ul id="post-comments-${post._id}">
+                                    
+                                </ul>
+                        </div>
+        
+                    </div>
+                </li>
+                `)
     }
 
     createPost();
