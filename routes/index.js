26427d7abc3cfd3ab9ecf2b27c55a772b9653d3c
Sai prasad <sp@MacBook-Pro.local>
2021-02-13 16:13:24 +0530

    Set up directory structure for apis

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 // for any further routes,access from here
+
+router.use('/api',require('./api'));
+
 // router.use('/routerName',require('./routerfile));
 module.exports=router;
